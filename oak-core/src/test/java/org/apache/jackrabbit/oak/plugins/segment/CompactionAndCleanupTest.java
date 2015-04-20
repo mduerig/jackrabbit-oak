4544b831559f3439336e21d775fdeadcaea4ee29
Michael Duerig <mduerig@apache.org>
2015-04-20 12:30:03 +0200

    OAK-2801: Clear excess references before cleanup * Create a new TarWriter before cleanup * Flush segment cache in SegmentTracker before cleanup * Remove special casing of bulk segments in TarReader.cleanup as it might lead to SNFEs

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/segment/CompactionAndCleanupTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/segment/CompactionAndCleanupTest.java
@@ -162,7 +162,7 @@ public class CompactionAndCleanupTest {
             // fileStore.size() in [blobSize + dataSize, blobSize + 2xdataSize]
             assertTrue(fileStore.maybeCompact(false));
             fileStore.cleanup();
-            assertSize("post cleanup", fileStore.size(), blobSize + dataSize,
+            assertSize("post cleanup", fileStore.size(), (blobSize + dataSize) / 2,
                     blobSize + 2 * dataSize);
 
             // refresh the ts ref, to simulate a long wait time
