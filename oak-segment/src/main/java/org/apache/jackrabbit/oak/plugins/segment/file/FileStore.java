cb17bf0e6f65829a26f8e02548ffebae05f52f4f
Michael Duerig <mduerig@apache.org>
2016-03-01 17:43:42 +0100

    Id based equality: node states now carry an id. Optimise equality for blobs by checking equality of its block ids.

--- oak-segment/src/main/java/org/apache/jackrabbit/oak/plugins/segment/file/FileStore.java
+++ oak-segment/src/main/java/org/apache/jackrabbit/oak/plugins/segment/file/FileStore.java
@@ -1103,7 +1103,6 @@ public class FileStore implements SegmentStore {
         }
     }
 
-    // michid aqcuire exclusive lock
     private boolean forceCompact(NodeState before, SegmentNodeState onto, Compactor compactor)
             throws InterruptedException, IOException {
         if (rwLock.writeLock().tryLock(compactionStrategy.getLockWaitTime(), TimeUnit.SECONDS)) {
