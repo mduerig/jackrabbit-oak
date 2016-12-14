6751a60de7ff16423fce7f03e016797ea06654f2
Michael Duerig <mduerig@apache.org>
2016-12-14 15:45:12 +0100

    michid

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/MapEntry.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/MapEntry.java
@@ -100,7 +100,7 @@ class MapEntry extends AbstractChildNodeEntry
         return ComparisonChain.start()
                 .compare(getHash() & HASH_MASK, that.getHash() & HASH_MASK)
                 .compare(name, that.name)
-                .compare(value, that.value)
+                .compare(value, that.value)  // michid report issue
                 .result();
     }
 
