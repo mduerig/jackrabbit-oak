0d7321c185b8d8098ae6f3f4605c3443a3541832
Michael Duerig <mduerig@apache.org>
2017-04-11 17:05:51 +0200

    cleanup every minute

--- oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/SegmentCompactionIT.java
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/SegmentCompactionIT.java
@@ -161,7 +161,7 @@ public class SegmentCompactionIT {
     private volatile int nodeAddRatio = 40;
     private volatile int addStringRatio = 20;
     private volatile int addBinaryRatio = 0;
-    private volatile int compactionInterval = 60;
+    private volatile int compactionInterval = 1;
     private volatile boolean stopping;
     private volatile Reference rootReference;
     private volatile long fileStoreSize;
@@ -690,7 +690,7 @@ public class SegmentCompactionIT {
                         @Override
                         public Void call() throws Exception {
                             gcMonitor.resetCleaned();
-                            fileStore.getGCRunner().run();
+                            fileStore.cleanup();
                             return null;
                         }
                     });
